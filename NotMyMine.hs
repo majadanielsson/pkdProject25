7967e8b5ec2d6d2b5110468372eb257ac3e8491e
Martin Dannelind <36402231+GrannyPower@users.noreply.github.com>
2018-02-20 15:03:20 +0100

    Update NotMyMine.hs

--- NotMyMine.hs
+++ NotMyMine.hs
@@ -1,178 +1,259 @@
-import Control.Monad
+-- Project group 25; Minesweeper
+-- Erik Hellström, Maja Danielsson, Martin Dannelind
+
 import System.Random
-import Data.List
 import System.IO
-import Debug.Trace
+import Data.List
+import Control.Monad
+import System.Exit
 
-data State a = Mine | Unexplored | Clue a deriving(Show)
+-----------------------------------------------------------------------------------------------------------------------------
 
-type Point    = (Int, Int)
-type World    = [[State Int]]
-type ClueMap  = [[Int]]
-type Explored = [[State Int]]
+{- Cell is what each coordinate in the game is. It is either a Unexplored cell, a mine or a clue. 
+  INVARIANT: ????????????????
+-}
+data Cell = Unexplored | Mine | Clue Int deriving (Show, Eq)
 
-size   = 2  -- the size of each cell
-width  = 30 -- the width of the board
-height = 30 -- the height of the board
+-- Coords tells the position of each cell in the game.
+type Coords = (Int, Int)
+-- Board is the game field that is made up by a list of tuples containing a cell with its coordinate and data Cell value.
+type Board = [(Coords, Cell)]
+-- MineTracker is a list of coordinates for the location of the mines on the board.
+type MineTracker = [(Int, Int)]
 
-{- We start the program by first creating
-   a RandomGen which is used as a seed for planting mines.
-   Then we create two World's explored and world,
-     - explored will be what is displayed
-     - world will store the mine locations
-   We then start the game -}
+-----------------------------------------------------------------------------------------------------------------------------
+
+{- main
+   A brief human-readable description of the purpose of the function.
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
 main :: IO ()
 main = do
-  g        <- getStdGen
-  explored <- return $ makeMatrix width height Unexplored -- nothing is explored
-  world    <- return $ genGame width height (width * height `div` 10) g
-  runGame explored world >>= showWorld
+  putStrLn "Let's play MineSweeper"
+  gamePlay unexploredBoard facit
+  putStrLn "Game Over"
 
-{- This is the game loop,
-     - we first draw the board
-     - then get the move from the player
-     - then explore the world at the players move.
-   The game ends if a mine is visible after a move.
-   The loop happens by recursively calling runGame
-   with the new explored board -}
-runGame :: Explored -> World -> IO Explored
-runGame e w = do
-  showWorld e
-  move <- getLine
-  newE <- return $ explore w (parseInput move) e
-  case isMineVisible newE of
-      True  -> return newE
-      False -> runGame newE w
+{- gamePlay
+   A brief human-readable description of the purpose of the function.
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+gamePlay :: Board -> Board -> IO ()
+gamePlay gameBoard solution
+  | filter (\((x,y), c) -> c /= Mine) solution == filter (\((x,y), c) -> c /= Unexplored) gameBoard = do
+    putStrLn "YOU WON!"
+    putStrLn ""
+    playAgain
+  | gameBoard == solution = do
+    printBoard solution
+    putStrLn ""
+    putStrLn "You a loser LOL XD"
+    putStrLn ""
+    playAgain
+  | otherwise = do
+    printBoard gameBoard
+    putStrLn "Which cell do you want to reveal?"
+    putStrLn "Letter? (lower case)"
+    y <- getLine
+    putStrLn "Number?"
+    x <- getLine
+    gamePlay (gamePlayAux gameBoard solution (read x) (charToInt y)) solution
 
-{- There are two cases we care about when exploring a move,
-     - The point is unexplored, in which case we explore it
-     - it has already been explored, so we don't have to do anything -}
-explore :: World -> Point -> Explored -> Explored
-explore w p@(x, y) e = case e !! x !! y of
-                         Unexplored -> updateExplored w p e
-                         _          -> e
+{- gamePlayAux b1 b2 x y
+   Reveals what is hidden at a given coordinate on the board
+   PRE: True
+   RETURNS: b1 with the coordinate (x,y) changed based on b2
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+gamePlayAux :: Board -> Board -> Int -> Int -> Board
+gamePlayAux gameBoard solution x y
+                     | 19 < x || x < 0 || 19 < y || y < 0 = gameBoard
+                     -- | snd (head (gamePlayAux' (x,y) solution)) == Clue 0 =
+                     | snd (head (gamePlayAux' (x,y) solution)) == Mine = solution
+                     | otherwise = insertCells (gamePlayAux' (x,y) solution) gameBoard
+                        where
+                          gamePlayAux' (x,y) [] = [((x,y), Unexplored)]
+                          gamePlayAux' (x,y) (((a,b), c):as)
+                              | (x,y) == (a,b) = [((x,y), c)]
+                              | otherwise = gamePlayAux' (x,y) as
+{- playAgain
+   A brief human-readable description of the purpose of the function.
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+playAgain :: IO ()
+playAgain = do
+  putStrLn ""
+  putStrLn "Want to play again? y or n"
+  putStrLn ""
+  ans <- getLine
+  if ans == "y"
+  then do main
+  else do exitFailure
 
-{- To update the explored map, we copy the state
-   from the world to the explored map.
-   If the state is Clue 0, we explore the
-   surrounding cells to save the player the
-   mindless job of exploring those manually -}
-updateExplored :: World -> Point -> Explored -> Explored
-updateExplored w p@(x, y) e =
-    case state of
-      Clue 0 -> foldr (explore w) newExplored $ surrounding width height p
-      _      -> newExplored
-    where
-      state       = (w !! x !! y)
-      newExplored = replaceMatrixIndex p e state
+{- charToInt s
+   Maps each letter on the board to its corresponding integer coordinate
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+charToInt :: String -> Int
+charToInt str
+  |str=="a"=0|str=="b"=1|str=="c"=2|str=="d"=3|str=="e"=4|str=="f"=5|str=="g"=6|str=="h"=7
+  |str=="i"=8|str=="j"=9|str=="k"=10|str=="l"=11|str=="m"=12|str=="n"=13|str=="o"=14|str=="p"=15
+  |str=="q"=16|str=="r"=17|str=="s"=18|str=="t"=19| otherwise = 21
 
-{- Checks if a mine is visible in the explored map -}
-isMineVisible :: Explored -> Bool
-isMineVisible [] = False
-isMineVisible (xs:xss)
-              | containsMine xs = True
-              | otherwise       = isMineVisible xss
-              where containsMine xs = not $ all notMine xs
-                    notMine Mine    = False
-                    notMine _       = True
+{- printBoard b
+   Prints the board in the terminal with underlaying letters and numbers on the right side to help the user to point
+   out the right coordinate.
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+printBoard :: Board -> IO ()
+printBoard playerBoard = printBoard' ((divideBoardString (stringBoard playerBoard) 0)
+                         ++ ["____________________________________________________________"]
+                         ++ [" a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t "])
+                where
+                  printBoard' (x:xs) = mapM_ print (x:xs)
 
-{- Parses the user input into a point -}
-parseInput :: String -> Point
-parseInput line = listToPair $ map read $ words line
-    where listToPair (x:y:_) = (x, y)
+{- stringBoard b
+   Converts all cells in a board into how they are to be displayed on the game board
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: b where all elements are converted from type Board into how they are to
+            be displayed on the game board
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+stringBoard :: Board -> String
+stringBoard [] = []
+stringBoard (x:xs) = showCell (snd x) ++ stringBoard xs
 
-{- Generates a world of dimensions w, h, with n mines scattered
-   randomly throughout and all the clues numbers calculated -}
-genGame :: RandomGen g => Int -> Int -> Int -> g -> World
-genGame w h n g = [zipWith combine ms cs | (ms, cs) <- zip mineMap clueMap]
-                   where
-                     mines   = nub $ genPoints w h n g
-                     clueMap = genClueMap w h mines
-                     mineMap = genWorld w h n mines
-                     combine :: (State a) -> a -> State a
-                     combine Mine _       = Mine
-                     combine Unexplored _ = Unexplored
-                     combine (Clue _) x   = Clue x
+{- divideBoardString s acc
+   Divides a string into groups of 60 and ends each new string with a boarder what number
+   string it is, starting at 0
+   PRE: True
+   RETURNS: s divided into acc+1 groups of 60
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+divideBoardString :: String -> Int -> [String]
+divideBoardString [] b = []
+divideBoardString xs b = [(take 60 xs) ++ "|" ++ (show b)] ++ divideBoardString (drop 60 xs) (b+1)
 
-{- Generates a list of random points with in the box of dimensions w h -}
-genPoints :: RandomGen g => Int -> Int -> Int -> g -> [Point]
-genPoints w h n g = zip xs ys
-    where
-      xs = take n (randomRs (0, w-1) g)
-      ys = drop n $ take (n*2) $ randomRs (0, h-1) g
+{- showCell cell
+   Converts a cell into how it is to be displayed on the game board
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+showCell :: Cell -> String
+showCell (Clue 0)   = "   "
+showCell (Clue n)   = " " ++ (show n) ++ " "
+showCell Unexplored = " = "
+showCell Mine       = " x "
 
-{- Generates a world of dimensions w h and mines randomly scattered -}
-genWorld :: Int -> Int -> Int -> [Point] -> World
-genWorld w h n mines = foldr placeMine world mines
-    where
-      world             = makeMatrix w h (Clue 0) -- Initial world has no mines
-      placeMine p world = replaceMatrixIndex p world Mine
+{- emptyBoard 
+   Creates a board consisting of only empty cells (Clue 0)
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+emptyBoard :: Board
+emptyBoard = emptyBoard' 0 0
+              where
+                emptyBoard' :: Int -> Int -> Board
+                -- VARIANT: value x, value y
+                emptyBoard' 20 y = []
+                emptyBoard' x 19 = [((x,19), Clue 0)] ++ emptyBoard' (x+1) 0
+                emptyBoard' x y = [((x,y), Clue 0)] ++ emptyBoard' x (y+1)
 
-{- Generates a map with the clue numbers in each cell representing
-   the number of adjacent to mines -}
-genClueMap :: Int -> Int -> [Point] -> ClueMap
-genClueMap w h mines = foldr succPoint clueMap surroundingPoints
-    where
-      surroundingPoints          = concat $ map (surrounding w h) mines
-      clueMap                    = replicate w $ replicate h 0
-      succPoint p@(x, y) clueMap = replaceMatrixIndex p clueMap
-                                   $ succ (clueMap !! x !! y)
+{- unexploredBoard 
+   Creates a board consisting of only unexplored cells (Unexplored)
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+unexploredBoard :: Board
+unexploredBoard = unexploredBoard' 0 0
+              where
+                unexploredBoard' :: Int -> Int -> Board
+                --VARIANT: value x, value y 
+                unexploredBoard' 20 y = []
+                unexploredBoard' x 19 = [((x,19), Unexplored)] ++ unexploredBoard' (x+1) 0
+                unexploredBoard' x y = [((x,y), Unexplored)] ++ unexploredBoard' x (y+1)
 
-{- Finds all points surrounding a point with all
-   points outside of the bounds w h filtered out -}
-surrounding :: Int -> Int -> Point -> [Point]
-surrounding w h (x, y) =
-    filter (inBounds w h) [(x-1, y+1), (x, y+1), (x+1, y+1),
-                           (x-1, y),             (x+1, y),
-                           (x-1, y-1), (x, y-1), (x+1, y-1)]
+{- randomMineCells
+   Creates a board of 14 (not so random) mines...
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: randomMineCells = [((0,2), Mine), ((4,6),Mine),((4,5), Mine),((5,7), Mine)]
+-}
+randomMineCells :: Board
+randomMineCells = [((0,2), Mine), ((4,6),Mine),((4,5), Mine),((5,7), Mine),((1,6), Mine),((8,2), Mine),
+                   ((9,9), Mine), ((11,12), Mine), ((14,16),Mine),((14,15), Mine),((15,17), Mine),
+                   ((11,16), Mine),((18,12), Mine),((19,19), Mine)]
 
-{- Prints out the world -}
-showWorld ::  World -> IO ()
-showWorld w = putStrLn $ showMatrixWith showSquare w
+{- insertCells b newB
+   A brief human-readable description of the purpose of the function.
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+insertCells :: Board -> Board -> Board
+insertCells [] playboard = playboard
+insertCells (x:xs) playboard = insertCells xs (insertCells' x playboard)
+                     where
+                      insertCells' ((x,y), c1) (((a,b), c2):as)
+                          | (x,y) == (a,b) = (((a,b), c1):as)
+                          | otherwise = ((a,b), c2): insertCells' ((x,y), c1) as
 
-showSquare :: State Int -> String
-showSquare Mine       = showCentered size "*"
-showSquare Unexplored = showCentered size "#"
-showSquare (Clue 0)   = showCentered size " "
-showSquare (Clue n)   = showCentered size (show n)
+{- trackMines b
+   Computes the coordinates adjacent to the coordinates of the cells on the board
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+trackMines :: Board -> MineTracker
+trackMines playboard =  filter (\(x,y) -> x /= -1 && x /= 20 && y /= -1 && y /= 20) (trackMines' playboard)
+                where
+                 trackMines' [] = []
+                 trackMines' (((x,y), _ ):xs) =
+                     trackMines' xs ++ [(x+1, y-1), (x, y-1), (x-1, y-1),(x+1, y),
+                                       (x-1, y), (x+1, y+1), (x, y+1), (x-1, y+1)]
 
-showCentered :: Int -> String -> String
-showCentered w x = (replicate leftPad ' ') ++ x ++ (replicate rightPad ' ')
-    where leftPad  =  w `div` 2
-          rightPad =  w - leftPad - (length x)
+{- insertTrack mineT b
+   A brief human-readable description of the purpose of the function.
+   PRE:  ... pre-condition on the arguments, if any ...
+   RETURNS: ... description of the result, in terms of the arguments ...
+   SIDE EFFECTS: ... side effects, if any, including exceptions ...
+   EXAMPLES: ... especially if useful to highlight delicate issues; also consider including counter-examples ...
+-}
+insertTrack :: MineTracker -> Board -> Board
+insertTrack [] playboard = playboard
+insertTrack ((x,y):xs) playboard = insertTrack xs (insertTrack' (x,y) playboard)
+                    where
+                      insertTrack' (x,y) (((a,b), Mine):as)
+                            | (x,y) == (a,b) = (((a,b), Mine):as)
+                            | otherwise = ((a,b), Mine): insertTrack' (x,y) as
+                      insertTrack' (x,y) (((a,b), Clue c):as)
+                            | (x,y) == (a,b) = (((a,b), Clue (c+1)):as)
+                            | otherwise = ((a,b), Clue c): insertTrack' (x,y) as
 
-{- Maps a function over a list of lists -}
-matrixMap :: (a -> b) -> [[a]] -> [[b]]
-matrixMap f xss = map (map f) xss
-
-showMatrixWith :: (a -> String) -> [[a]] -> String
-showMatrixWith f = unlines . addBorder . map concat . matrixMap f . transpose
-
-{- Adds a border around a list of strings -}
-addBorder :: [String] -> [String]
-addBorder xs = [horizontalBorder w]
-               ++ map verticalBorder xs
-               ++ [(horizontalBorder w)]
-    where w                  = length (xs !! 0)
-          h                  = length xs
-          horizontalBorder w = "+" ++ (replicate w '-') ++ "+"
-          verticalBorder xs  = "|" ++ xs ++ "|"
-
-replaceMatrixIndex :: Point -> [[a]] -> a -> [[a]]
-replaceMatrixIndex (x, y) m e = replaceIndex x m $ replaceIndex y (m !! x) e
-
-{- Replaces an element at an index in a list with another element -}
-replaceIndex :: Int -> [a] -> a -> [a]
-replaceIndex index xs x = take index xs ++ ( x : (drop (index+1) xs))
-
-{- Creates a matrix fill with a given element -}
-makeMatrix :: Int -> Int -> a -> [[a]]
-makeMatrix w h e = replicate w $ replicate h e
-
-inBounds :: Int -> Int -> Point -> Bool
-inBounds w h (x, y)
-    | x < 0     = False
-    | x >= w    = False
-    | y < 0     = False
-    | y >= h    = False
-    | otherwise = True
+facit = (insertTrack (trackMines randomMineCells) (insertCells randomMineCells emptyBoard))
